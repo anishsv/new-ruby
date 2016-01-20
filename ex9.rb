998c9e1b23bbc1611f0324fc3a3aadc4ebe3ec6d
svanish <anishsv2009@gmail.com>
2016-01-20 15:53:29 +0530

    escape sequence and multiline string

---
+++ ex9.rb
@@ -0,0 +1,13 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days:" + days
+puts "Here are the months: #{months}"
+
+puts """
+This is a 
+multiline string
+for reference
+"""
