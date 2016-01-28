835fe62afb9555be566019186326d56399eb0c9c
svanish <anishsv2009@gmail.com>
2016-01-28 15:46:36 +0530

    If condition

---
+++ ex29.rb
@@ -0,0 +1,21 @@
+cat = 30
+dog = 50
+people = 60
+
+if people > dog
+	puts "More people"
+end
+
+if cat < dog
+	puts "More dogs than cat"
+end
+
+dog += 10
+
+if people >= dog
+	puts "peoples are same as dog"
+end
+
+if people == dog
+	puts "Peoples are dogs"
+end
